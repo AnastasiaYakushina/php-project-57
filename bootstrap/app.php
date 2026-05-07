f75bb9201ee3035e74824dc2aa98e929e2477137
AnastasiaYakushina <aubarr@ya.ru>
2026-05-07 21:44:32 +0300

    Add Sentry integration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,9 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function () {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function () {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        Integration::handles($exceptions);
     })->create();
